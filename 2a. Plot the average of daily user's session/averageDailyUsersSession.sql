303422bac142a19c060c81df38ecc18ac97a1527
nuhadi <nuhadinata@gmail.com>
2021-01-10 19:20:25 +0700

    Update averageDailyUsersSession.sql

--- 2a. Plot the average of daily user's session/averageDailyUsersSession.sql
+++ 2a. Plot the average of daily user's session/averageDailyUsersSession.sql
@@ -3,5 +3,7 @@ SELECT
   AVG(TIMESTAMP_DIFF(end_time, start_time, second)) as average_time
 FROM
   `bonza-interview.analyst.telecom_data`
-GROUP BY day
-ORDER BY day;
+GROUP BY 
+  day
+ORDER BY
+  day;
